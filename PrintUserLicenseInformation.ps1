580acc3ef35adf275852f43dcc5293066cfa1fd6
Axio112 <104569640+Axio112@users.noreply.github.com>
2024-07-28 19:21:14 +0300

    Add files via upload

---
+++ PrintUserLicenseInformation.ps1
@@ -0,0 +1,17 @@
+﻿Connect-AzureAD
+# Fetch all SKUs
+$skus = Get-AzureADSubscribedSku
+
+# Display SKUs
+$skus | ForEach-Object {
+    Write-Host "SKU ID: $($_.SkuId) - SKU Part Number: $($_.SkuPartNumber)"
+}
+
+# Fetch a user's license information
+$user = Get-AzureADUser -ObjectId "UPN"
+$licenses = Get-AzureADUserLicenseDetail -ObjectId $user.ObjectId
+
+# Display user license information
+$licenses | ForEach-Object {
+    Write-Host "Service Plan: $($_.ServicePlans)"
+}
